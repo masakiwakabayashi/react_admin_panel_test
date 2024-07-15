67ba48a8b64df6879100442768da2f5bfbcc84a3
若林将輝 <masaki.wakabaya@gmail.com>
2024-07-15 16:23:32 +0900

    create topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { Admin, Resource } from "react-admin";
-import jsonServerProvider from "ra-data-json-server";
-import { UserList } from "./components/Users";
-import { PostList } from "./components/Post";
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+import Topbar from "./components/common/molecules/Topbar";
+
 
 const App = () => (
   <div className="app">
-    <Admin dataProvider={dataProvider}>
-      <Resource name="users" list={UserList} />
-      <Resource name="posts" list={PostList} />
-    </Admin>
+    <main className="content">
+      <Topbar/>
+    </main>
+
   </div>
 );
 
--- src/components/common/molecules/Topbar.jsx
+++ src/components/common/molecules/Topbar.jsx
@@ -1,8 +1,39 @@
+import { Box, IconButton, InputBase } from '@mui/material';
+
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+
 const Topbar = () => {
   return (
-    <div>
-      Enter
-    </div>
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* Search bar */}
+      <Box
+        display="flex"
+        backgroundColor="white"
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="検索"/>
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      {/* Icons */}
+      <Box display="flex">
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
+      </Box>
+    </Box>
   );
 }
 
