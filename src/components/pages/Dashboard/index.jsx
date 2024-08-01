b96537fe2aa1e0d31bdecb29ac0a7dfee168b82e
若林将輝 <masaki.wakabaya@gmail.com>
2024-08-02 00:49:47 +0900

    create dashboard

--- src/components/pages/Dashboard/index.jsx
+++ src/components/pages/Dashboard/index.jsx
@@ -8,6 +8,8 @@ import TrafficIcon from "@mui/icons-material/Traffic";
 
 import StatBox from "../../molecules/StatBox";
 
+import { mockTransactions } from "../../../data/mockData";
+
 const Dashboard = () => {
   return (
     <Box m="20px">
@@ -110,14 +112,106 @@ const Dashboard = () => {
             }
           />
         </Box>
-
-
-
-
-
-
-
         {/* Row 2 */}
+        <Box
+          gridColumn="span 8"
+          gridRow="span 2"
+          backgroundColor="lightcyan"
+        >
+          <Box
+            mt="25px"
+            p="0 30px"
+            display="flex "
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Box>
+              <Typography
+                variant="h5"
+                fontWeight="600"
+                color="blue"
+              >
+                Revenue Generated
+              </Typography>
+              <Typography
+                variant="h4"
+                fontWeight="bold"
+                color="blue"
+              >
+                $59,342.32
+              </Typography>
+            </Box>
+            <Box>
+              <IconButton>
+                <DownloadOutlinedIcon
+                  sx={{ fontSize: "26px", color: "blue" }}
+                />
+              </IconButton>
+            </Box>
+          </Box>
+          <Box height="250px" m="-20px 0 0 0">
+            {/* <LineChart isDashboard={true} /> */}
+          </Box>
+        </Box>
+
+
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor="lightcyan"
+          overflow="auto"
+        >
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            borderBottom={`4px solid blue`}
+            colors="lightcyan"
+            p="15px"
+          >
+            <Typography color="blue" variant="h5" fontWeight="600">
+              Recent Transactions
+            </Typography>
+          </Box>
+          {mockTransactions.map((transaction, i) => (
+            <Box
+              key={`${transaction.txId}-${i}`}
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              borderBottom={`4px solid blue`}
+              p="15px"
+            >
+              <Box>
+                <Typography
+                  color="blue"
+                  variant="h5"
+                  fontWeight="600"
+                >
+                  {transaction.txId}
+                </Typography>
+                <Typography color="blue">
+                  {transaction.user}
+                </Typography>
+              </Box>
+              <Box color="blue">{transaction.date}</Box>
+              <Box
+                backgroundColor="lightcyan"
+                p="5px 10px"
+                borderRadius="4px"
+              >
+                ${transaction.cost}
+              </Box>
+            </Box>
+          ))}
+        </Box>
+
+
+
+
+
+
+
 
 
 
